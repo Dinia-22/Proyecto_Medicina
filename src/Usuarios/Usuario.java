8b5ad6dc6cab2410aa9eaff1be9a111c6450baed
Maria Paula <Maria Paula@Maria>
2020-04-03 15:59:53 -0600

    //programar

--- src/Usuarios/Usuario.java
+++ src/Usuarios/Usuario.java
@@ -123,7 +123,6 @@ public class Usuario {
         try {
             PreparedStatement sentencia;
             sentencia = conexion.prepareStatement("insert usuario values(null,?,?,?,?,?,?,?)");
-
             sentencia.setString(1, "paula");
             sentencia.setInt(2, 2);
             sentencia.setString(3, "@");
@@ -132,14 +131,14 @@ public class Usuario {
             sentencia.setString(6, "medico");
             sentencia.setInt(7, 72031697);
             sentencia.execute();
-            
+
             sentencia.setString(1, "Katty");
             sentencia.setInt(2, 4);
             sentencia.setString(3, "Katty@hotmail.com");
             sentencia.setString(4, "ka56");
             sentencia.setString(5, "19967");
             sentencia.setString(6, "secretaria");
-            sentencia.setInt(7,234510);
+            sentencia.setInt(7, 234510);
             sentencia.execute();
 
         } catch (SQLException ex) {
@@ -151,26 +150,128 @@ public class Usuario {
     public void update(int Id, String Nombre, int Fecha, String Correo, String Usuario, String Tipo, int Tel) {
         try {
             this.sentencias.executeUpdate("update usuario set nombre='" + Nombre + "',fecha='" + Fecha + "',correo='" + Correo + "',usuario='" + Usuario + "',tipo='" + Tipo + "',telefono='" + Tel + "' where id=" + Id);
-            
+
         } catch (SQLException ex) {
             Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
         }
 
     }
-    
-    public void delete(int Id){ /// controlar que siempre quede un usuario registrado al momento de eliminar
+
+    public void delete(int Id) { /// controlar que siempre quede un usuario registrado al momento de eliminar
         try {
-            this.sentencias.executeUpdate("delete from usuario where id="+Id);
+            this.sentencias.executeUpdate("delete from usuario where id=" + Id);
         } catch (SQLException ex) {
             System.out.println("Error en delete");
         }
+    }
+ ///////////////////////////// para buscar la informacion del usuario///////////////////////////////////////////////////
+    public void readNombre(String Nombre) {
+        try {
+            this.datos = this.sentencias.executeQuery("select * from usuario where Nombre='" + Nombre + "'");
+            if (this.datos.next()) {
+                System.out.println(datos.getInt(1));
+                System.out.println(datos.getString(2));
+                System.out.println(datos.getString(3));
+            } else {
+                System.out.println(" No hay mas registros ");
+            }
+        } catch (SQLException ex) {
+            System.out.println(" Error en el read");
+        }
+
+    }
+
+    public void readFecha(int Fecha) {
+        try {
+            this.datos = this.sentencias.executeQuery("select * from usuario where Nombre='" + Fecha + "'");
+            if (this.datos.next()) {
+                System.out.println(datos.getInt(1));
+                System.out.println(datos.getString(2));
+                System.out.println(datos.getString(3));
+            } else {
+                System.out.println(" No hay mas registros ");
+            }
+        } catch (SQLException ex) {
+            System.out.println(" Error en el read");
+        }
+
+    }
+
+    public void readCorreo(String Correo) {
+        try {
+            this.datos = this.sentencias.executeQuery("select * from usuario where Nombre='" + Correo + "'");
+            if (this.datos.next()) {
+                System.out.println(datos.getInt(1));
+                System.out.println(datos.getString(2));
+                System.out.println(datos.getString(3));
+            } else {
+                System.out.println(" No hay mas registros ");
+            }
+        } catch (SQLException ex) {
+            System.out.println(" Error en el read");
+        }
+
+    }
+
+    public void readUsuario(String Usuario) {
+        try {
+            this.datos = this.sentencias.executeQuery("select * from usuario where Nombre='" + Usuario + "'");
+            if (this.datos.next()) {
+                System.out.println(datos.getInt(1));
+                System.out.println(datos.getString(2));
+                System.out.println(datos.getString(3));
+            } else {
+                System.out.println(" No hay mas registros ");
+            }
+        } catch (SQLException ex) {
+            System.out.println(" Error en el read");
+        }
+
+    }
+
+    public void readTipo(String Tipo) {
+        try {
+            this.datos = this.sentencias.executeQuery("select * from usuario where Nombre='" + Tipo + "'");
+            if (this.datos.next()) {
+                System.out.println(datos.getInt(1));
+                System.out.println(datos.getString(2));
+                System.out.println(datos.getString(3));
+            } else {
+                System.out.println(" No hay mas registros ");
+            }
+        } catch (SQLException ex) {
+            System.out.println(" Error en el read");
+        }
+
+    }
+
+    public void readTel(int Tel) {
+        try {
+            this.datos = this.sentencias.executeQuery("select * from usuario where Nombre='" + Tel + "'");
+            if (this.datos.next()) {
+                System.out.println(datos.getInt(1));
+                System.out.println(datos.getString(2));
+                System.out.println(datos.getString(3));
+            } else {
+                System.out.println(" No hay mas registros ");
+            }
+        } catch (SQLException ex) {
+            System.out.println(" Error en el read");
+        }
+
     }
 
     public static void main(String[] args) {
         // TODO code application logic here
         Usuario p = new Usuario(0, "", 0, 0, "", "", "", "");
         p.conectar();
-        p.delete(1);
+        p.readNombre("paula");
+        p.readCorreo("");
+        p.readFecha(0);
+        p.readTel(0);
+        p.readTipo("");
+        p.readUsuario("");  
+        //p.delete(1);
         //p.update(1, "maria", 4, "hotmail", "p123", "secretaria", 01234);
         //p.create();
 
