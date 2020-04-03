a308705056ec8a7f6ffdf25da4f68a0baf5ab749
Maria Paula <Maria Paula@Maria>
2020-04-03 15:34:37 -0600

    //programar

--- src/Usuarios/Usuario.java
+++ src/Usuarios/Usuario.java
@@ -120,18 +120,26 @@ public class Usuario {
 
     public void create() {
 
-        
         try {
             PreparedStatement sentencia;
             sentencia = conexion.prepareStatement("insert usuario values(null,?,?,?,?,?,?,?)");
-           
+
             sentencia.setString(1, "paula");
-            sentencia.setInt(2,2);
+            sentencia.setInt(2, 2);
             sentencia.setString(3, "@");
             sentencia.setString(4, "pau");
             sentencia.setString(5, "1234");
             sentencia.setString(6, "medico");
             sentencia.setInt(7, 72031697);
+            sentencia.execute();
+            
+            sentencia.setString(1, "Katty");
+            sentencia.setInt(2, 4);
+            sentencia.setString(3, "Katty@hotmail.com");
+            sentencia.setString(4, "ka56");
+            sentencia.setString(5, "19967");
+            sentencia.setString(6, "secretaria");
+            sentencia.setInt(7,234510);
             sentencia.execute();
 
         } catch (SQLException ex) {
@@ -140,11 +148,31 @@ public class Usuario {
 
     }
 
+    public void update(int Id, String Nombre, int Fecha, String Correo, String Usuario, String Tipo, int Tel) {
+        try {
+            this.sentencias.executeUpdate("update usuario set nombre='" + Nombre + "',fecha='" + Fecha + "',correo='" + Correo + "',usuario='" + Usuario + "',tipo='" + Tipo + "',telefono='" + Tel + "' where id=" + Id);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+    
+    public void delete(int Id){ /// controlar que siempre quede un usuario registrado al momento de eliminar
+        try {
+            this.sentencias.executeUpdate("delete from usuario where id="+Id);
+        } catch (SQLException ex) {
+            System.out.println("Error en delete");
+        }
+    }
+
     public static void main(String[] args) {
         // TODO code application logic here
         Usuario p = new Usuario(0, "", 0, 0, "", "", "", "");
         p.conectar();
-        p.create();
+        p.delete(1);
+        //p.update(1, "maria", 4, "hotmail", "p123", "secretaria", 01234);
+        //p.create();
 
     }
 
