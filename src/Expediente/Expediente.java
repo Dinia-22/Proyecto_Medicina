cc7e4af3c21d2679cb7e535ec23d447ac42c163f
Adriel Chavarría B <Adriel Chavarra B@DESKTOP-GM47FTQ>
2020-04-07 19:34:48 -0600

    Mi primer comentario

--- src/Expediente/Expediente.java
+++ src/Expediente/Expediente.java
@@ -5,8 +5,8 @@
  */
 package Expediente;
 
-
 import Usuarios.Usuario;
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -14,31 +14,70 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Text;
 
 /**
  *
  * @author Maria Paula
  */
 public class Expediente {
-    private int Fecha;
-    private int hora;
+
+    private String Fecha;
+    private String hora;
     private String medico;
     private String Descrip;
+    private String paciente;
     private Connection conexion;
     private Statement sentencias;
 
-    public Expediente(int Fecha, int hora, String medico, String Descrip) {
-        this.Fecha = Fecha;
+//    public Expediente(int Fecha, int hora, String medico, String Descrip, String paciente, Connection conexion, Statement sentencias) {
+//        this.Fecha = Fecha;
+//        this.hora = hora;
+//        this.medico = medico;
+//        this.Descrip = Descrip;
+//        this.paciente = paciente;
+//        this.conexion = conexion;
+//        this.sentencias = sentencias;
+//    }
+
+    public Expediente() {
+       this.Fecha = Fecha;
         this.hora = hora;
         this.medico = medico;
         this.Descrip = Descrip;
+        this.paciente = paciente;
+        this.conexion = conexion;
+        this.sentencias = sentencias;
     }
 
-    public int getFecha() {
+   
+
+    
+
+    
+
+   
+    
+    public String getFecha() {
         return Fecha;
     }
 
-    public int getHora() {
+    public String getHora() {
         return hora;
     }
 
@@ -50,11 +89,11 @@ public class Expediente {
         return Descrip;
     }
 
-    public void setFecha(int Fecha) {
+    public void setFecha(String Fecha) {
         this.Fecha = Fecha;
     }
 
-    public void setHora(int hora) {
+    public void setHora(String hora) {
         this.hora = hora;
     }
 
@@ -66,6 +105,15 @@ public class Expediente {
         this.Descrip = Descrip;
     }
     
+    
+     public String getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(String paciente) {
+        this.paciente = paciente;
+    }
+
     public void conectar() {
         try {
             this.conexion = DriverManager.getConnection("jdbc:mysql://localhost/medicina?useServerPrepStmts=true", "root", "");
@@ -83,8 +131,8 @@ public class Expediente {
             sentencia = conexion.prepareStatement("insert expediente values(null,?,?,?,?)");
             sentencia.setInt(1, 5);
             sentencia.setInt(2, 2);
-            sentencia.setString(3,"Carlos");
-            sentencia.setString(4,"pierna rota");
+            sentencia.setString(3, "Carlos");
+            sentencia.setString(4, "pierna rota");
             sentencia.execute();
 
         } catch (SQLException ex) {
@@ -92,14 +140,68 @@ public class Expediente {
         }
 
     }
+//"C:\\Users\\Adriel Chavarría B\\Desktop"
+    public void xmlconfig() {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            DOMImplementation implementation = builder.getDOMImplementation();
 
-    public static void main(String[] args) {
-        // TODO code application logic here
-        Expediente p = new Expediente(0,0,"","");
-        p.conectar();
-        p.create();
-    
+            Document documento = implementation.createDocument(null, "consultorio.xml",null);
+            documento.setXmlVersion("1.0");
+            
+            Element archxml = documento.createElement("Consulta");
+            Element arch = documento.createElement("Consulta");
+            
+            Element hora = documento.createElement("Hora");
+            Text texHora=documento.createTextNode(""+this.getHora());
+            hora.appendChild(texHora);
+            archxml.appendChild(hora);
+            
+            Element fecha = documento.createElement("Fecha");
+            Text texFecha=documento.createTextNode(""+this.getFecha());
+            fecha.appendChild(texFecha);
+            archxml.appendChild(fecha);
+
+            Element medicos = documento.createElement("Medico");
+            Text textMedico;
+            textMedico = documento.createTextNode(""+this.getMedico());
+            medicos.appendChild(textMedico);
+            archxml.appendChild(medicos);
+            
+            Element pacientes = documento.createElement("Paciente");
+            Text texPaciente=documento.createTextNode(""+this.getPaciente());
+            pacientes.appendChild(texPaciente);
+            archxml.appendChild(pacientes);
+            
+            Element descripcion = documento.createElement("Descripcion");
+            Text texDescripcion=documento.createTextNode(""+this.getDescrip());
+            descripcion.appendChild(texDescripcion);
+            archxml.appendChild(descripcion);
+            
+            arch.appendChild(archxml);
+            
+            documento.getDocumentElement().appendChild(arch);
+            
+            Source source = new DOMSource(documento);
+            Result result = new StreamResult(new File("consultorio.xml"));
+
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.transform(source, result);
+
+        } catch (ParserConfigurationException | TransformerException | DOMException e) {
+            //System.out.println(e.getMessage());
+            JOptionPane.showMessageDialog(null, "Ah sido efectuada");
+        }
     }
-    
-    
+
+//    public static void main(String[] args) {
+//        // TODO code application logic here
+//        Expediente p = new Expediente();
+//        p.conectar();
+//        p.create();
+//        p.xmlconfig();
+//
+//    }
+
 }
