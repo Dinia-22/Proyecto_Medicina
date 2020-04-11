6be4e2e60bb843a474a8a5c0c80fe4065e9d22fb
Maria Paula <Maria Paula@Maria>
2020-04-11 14:47:25 -0600

    //programar

--- src/Medicos/Medicos.java
+++ src/Medicos/Medicos.java
@@ -314,7 +314,7 @@ public class Medicos {
             if (this.Nombre.getText().length() < 25 || this.tel.getText().length() < 10 || this.correo.getText().length() < 30 || this.Codigo.getText().length() < 7 || this.especialidad.getText().length() < 15) {
                 SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd");
                 this.sentencias.executeUpdate("update medicos set NombreCompleto='" + this.Nombre.getText() + "',FechaNacimiento='" + dt.format(this.FechaNacimiento.getDate()) + "',Telefono='" + this.tel.getText() + "',CorreoElectronico='" + this.correo.getText() + "',Codigo='" + this.Codigo.getText() + "',Especialidad='" + this.especialidad.getText() + "',Salario='" + this.salario.getText() + "' where id=" + this.ID.getText());
-                JOptionPane.showMessageDialog(null, "Se agregaron los datos");
+                JOptionPane.showMessageDialog(null, "Se actualizaron los datos");
 
             } else {
                 JOptionPane.showMessageDialog(null, "Paso el limite de caracteres");
