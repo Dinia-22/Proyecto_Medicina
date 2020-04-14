e72cb603f447ac4945ff0e001460fa3165545a7b
Dinia Alvarado <Dinia Alvarado@DESKTOP-JQ96R3T>
2020-04-14 17:18:26 -0600

    //primer commit

---
+++ src/Conectar/Conectar.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Conectar;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Dinia Alvarado
+ */
+public class Conectar {
+
+    public static Connection conexion;
+    public static Statement sentencias;
+    public static ResultSet datos;
+
+    public void conectar() {
+        try {
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost/medicina?useServerPrepStmts=true", "root", "");
+            sentencias = conexion.createStatement();
+            System.out.println("Conectado");
+
+        } catch (SQLException ex) {
+
+            System.out.println(" Error al conectar");
+        }
+    }
+}
