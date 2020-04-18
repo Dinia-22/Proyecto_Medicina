c91ae59a8338d37d7d2ca2c9297c20e7fbda4b85
Dinia Alvarado <Dinia Alvarado@DESKTOP-JQ96R3T>
2020-04-18 17:23:04 -0600

    primer commit

--- src/Usuarios/BuscarUsuarios.java
+++ src/Usuarios/BuscarUsuarios.java
@@ -21,7 +21,7 @@ public class BuscarUsuarios extends javax.swing.JFrame {
 
     Usuario prueba = new Usuario();
     Conectar conec = new Conectar();
-    DefaultTableModel detamo;
+    DefaultTableModel usuario;
 
     /**
      * Creates new form BuscarUsuarios
@@ -34,8 +34,8 @@ public class BuscarUsuarios extends javax.swing.JFrame {
     public void configurarTabla() {
 
         String Titulos[] = new String[8];
-        detamo = new DefaultTableModel(null, Titulos);
-        tabla.setModel(detamo);
+        usuario = new DefaultTableModel(null, Titulos);
+        tabla.setModel(usuario);
     }
 
     /**
@@ -47,6 +47,15 @@ public class BuscarUsuarios extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        Filtrar_Usuarios = new javax.swing.JDialog();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel10 = new javax.swing.JLabel();
+        jLabel11 = new javax.swing.JLabel();
+        jLabel12 = new javax.swing.JLabel();
+        txtid = new javax.swing.JTextField();
+        txtnombre = new javax.swing.JTextField();
+        gaurdar_usuario = new javax.swing.JButton();
+        cancelar_usuario = new javax.swing.JButton();
         jTabbedPane1 = new javax.swing.JTabbedPane();
         jPanel1 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
@@ -76,10 +85,97 @@ public class BuscarUsuarios extends javax.swing.JFrame {
         jScrollPane3 = new javax.swing.JScrollPane();
         tabla = new javax.swing.JTable();
         jLabel9 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
+        buscar_filtro = new javax.swing.JButton();
         verdatos = new javax.swing.JButton();
         cancelar = new javax.swing.JButton();
+
+        jPanel2.setBackground(new java.awt.Color(204, 204, 204));
+
+        jLabel10.setFont(new java.awt.Font("sansserif", 1, 14)); // NOI18N
+        jLabel10.setText("Filtrar Usuario");
+
+        jLabel11.setFont(new java.awt.Font("sansserif", 1, 14)); // NOI18N
+        jLabel11.setText("ID:");
+
+        jLabel12.setFont(new java.awt.Font("sansserif", 1, 14)); // NOI18N
+        jLabel12.setText("Nombre Completo:");
+
+        gaurdar_usuario.setBackground(new java.awt.Color(102, 102, 102));
+        gaurdar_usuario.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/save.png"))); // NOI18N
+        gaurdar_usuario.setText("Guardar");
+        gaurdar_usuario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                gaurdar_usuarioActionPerformed(evt);
+            }
+        });
+
+        cancelar_usuario.setBackground(new java.awt.Color(0, 204, 255));
+        cancelar_usuario.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/cancelar.png"))); // NOI18N
+        cancelar_usuario.setText("Cancelar");
+        cancelar_usuario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cancelar_usuarioActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(jLabel11))
+                            .addComponent(jLabel12))
+                        .addGap(26, 26, 26)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(txtid, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtnombre, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(65, 65, 65)
+                        .addComponent(gaurdar_usuario)
+                        .addGap(50, 50, 50)
+                        .addComponent(cancelar_usuario))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(105, 105, 105)
+                        .addComponent(jLabel10)))
+                .addContainerGap(18, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel10)
+                .addGap(14, 14, 14)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel11)
+                    .addComponent(txtid, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(16, 16, 16)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel12)
+                    .addComponent(txtnombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 35, Short.MAX_VALUE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(gaurdar_usuario)
+                    .addComponent(cancelar_usuario))
+                .addGap(18, 18, 18))
+        );
+
+        javax.swing.GroupLayout Filtrar_UsuariosLayout = new javax.swing.GroupLayout(Filtrar_Usuarios.getContentPane());
+        Filtrar_Usuarios.getContentPane().setLayout(Filtrar_UsuariosLayout);
+        Filtrar_UsuariosLayout.setHorizontalGroup(
+            Filtrar_UsuariosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(Filtrar_UsuariosLayout.createSequentialGroup()
+                .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        Filtrar_UsuariosLayout.setVerticalGroup(
+            Filtrar_UsuariosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Buscar Informacion");
@@ -283,7 +379,7 @@ public class BuscarUsuarios extends javax.swing.JFrame {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(18, 18, 18)
                         .addComponent(txtcedula, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -319,12 +415,17 @@ public class BuscarUsuarios extends javax.swing.JFrame {
         ));
         jScrollPane3.setViewportView(tabla);
 
-        jLabel9.setFont(new java.awt.Font("sansserif", 0, 14)); // NOI18N
-        jLabel9.setText("Buscar por Filtro:");
+        jLabel9.setFont(new java.awt.Font("sansserif", 1, 18)); // NOI18N
+        jLabel9.setText("Datos Base de Datos");
 
-        jButton1.setBackground(new java.awt.Color(102, 102, 102));
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/lupa.png"))); // NOI18N
-        jButton1.setText("Buscar Filtro");
+        buscar_filtro.setBackground(new java.awt.Color(102, 102, 102));
+        buscar_filtro.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/lupa.png"))); // NOI18N
+        buscar_filtro.setText("Buscar Filtro");
+        buscar_filtro.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buscar_filtroActionPerformed(evt);
+            }
+        });
 
         verdatos.setBackground(new java.awt.Color(102, 102, 102));
         verdatos.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/binoculars.png"))); // NOI18N
@@ -349,39 +450,35 @@ public class BuscarUsuarios extends javax.swing.JFrame {
         filtroLayout.setHorizontalGroup(
             filtroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(filtroLayout.createSequentialGroup()
-                .addGap(14, 14, 14)
-                .addComponent(jLabel9)
-                .addGap(18, 18, 18)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 298, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(20, 20, 20))
-            .addGroup(filtroLayout.createSequentialGroup()
-                .addGap(30, 30, 30)
-                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 564, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(152, Short.MAX_VALUE))
-            .addGroup(filtroLayout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(verdatos)
-                .addGap(139, 139, 139)
-                .addComponent(cancelar)
-                .addGap(125, 125, 125))
+                .addGroup(filtroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(filtroLayout.createSequentialGroup()
+                        .addGap(45, 45, 45)
+                        .addComponent(verdatos)
+                        .addGap(18, 18, 18)
+                        .addComponent(buscar_filtro)
+                        .addGap(18, 18, 18)
+                        .addComponent(cancelar))
+                    .addGroup(filtroLayout.createSequentialGroup()
+                        .addGap(258, 258, 258)
+                        .addComponent(jLabel9))
+                    .addGroup(filtroLayout.createSequentialGroup()
+                        .addGap(21, 21, 21)
+                        .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 564, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(161, Short.MAX_VALUE))
         );
         filtroLayout.setVerticalGroup(
             filtroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, filtroLayout.createSequentialGroup()
-                .addGap(14, 14, 14)
-                .addGroup(filtroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel9)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1))
+                .addGap(18, 18, 18)
+                .addComponent(jLabel9)
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 229, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 85, Short.MAX_VALUE)
+                .addGap(39, 39, 39)
                 .addGroup(filtroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(verdatos)
-                    .addComponent(cancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(23, 23, 23))
+                    .addComponent(cancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(buscar_filtro, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(verdatos))
+                .addContainerGap(42, Short.MAX_VALUE))
         );
 
         jTabbedPane1.addTab("Filtro", filtro);
@@ -447,7 +544,7 @@ public class BuscarUsuarios extends javax.swing.JFrame {
         try {
             ResultSet rs = sentencias.executeQuery("SELECT * FROM usuarios");
             String Titulos[] = {"ID", "NombreCompleto", "FechaNacimiento", "Telefono", "CorreoElectronico","NombredeUsuario","Contraseña","TipodeUsuario"};
-            detamo = new DefaultTableModel(null, Titulos);
+            usuario = new DefaultTableModel(null, Titulos);
 
             String fila[] = new String[8];
             rs.beforeFirst();
@@ -463,9 +560,9 @@ public class BuscarUsuarios extends javax.swing.JFrame {
                 fila[5] = rs.getString("NombredeUsuario");
                 fila[6] = rs.getString("Contraseña");
                 fila[7] = rs.getString("TipodeUsuario");
-                detamo.addRow(fila);
+                usuario.addRow(fila);
             }
-            tabla.setModel(detamo);
+            tabla.setModel(usuario);
         } catch (SQLException ex) {
             //JOptionPane.showMessageDialog(null, "Error al extraer datos de la tabla");
             Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
@@ -481,6 +578,67 @@ public class BuscarUsuarios extends javax.swing.JFrame {
     private void cancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cancelarActionPerformed
         this.dispose();
     }//GEN-LAST:event_cancelarActionPerformed
+
+    private void gaurdar_usuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_gaurdar_usuarioActionPerformed
+        String id, nombre, fecha;
+        String sql;
+        int contador = 0;
+
+        try {
+
+            id = txtid.getText();
+            nombre = txtnombre.getText();
+
+            // Contruccion de la consulta sql select
+            sql = "SELECT * FROM usuarios";
+
+            if (!id.equals("")) {
+                contador++;
+                sql += " WHERE ID LIKE '%" + id + "%'";
+            }
+
+            if (!nombre.equals("")) {
+                contador++;
+                if (contador == 1) {
+                    sql += " WHERE NombreCompleto LIKE '%" + nombre + "%'";
+                } else {
+                    sql += " AND NombreCompleto LIKE '%" + nombre + "%'";
+                }
+            }
+
+            ResultSet rs = sentencias.executeQuery(sql);
+            String encabezado[] = {"ID", "NombreCompleto", "FechaNacimiento", "Telefono", "CorreoElectronico", "NombredeUsuario", "Contraseña", "TipodeUsuario"};
+            usuario = new DefaultTableModel(null, encabezado);
+
+            String fila[] = new String[8];
+            rs.beforeFirst();
+            while (rs.next()) {
+                fila[0] = rs.getString("ID");
+                fila[1] = rs.getString("NombreCompleto");
+                fecha = rs.getString("FechaNacimiento");
+                fila[2] = fecha.substring(8, 10) + "/" + fecha.substring(5, 7) + "/" + fecha.substring(0, 4);
+                fila[3] = rs.getString("Telefono");
+                fila[4] = rs.getString("CorreoElectronico");
+                fila[5] = rs.getString("NombredeUsuario");
+                fila[6] = rs.getString("Contraseña");
+                fila[7] = rs.getString("TipodeUsuario");
+                usuario.addRow(fila);
+            }
+            tabla.setModel(usuario);
+        } catch (Exception ex) {
+            Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_gaurdar_usuarioActionPerformed
+
+    private void buscar_filtroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buscar_filtroActionPerformed
+        Filtrar_Usuarios.setSize(350,230);
+        Filtrar_Usuarios.setModal(true);
+        Filtrar_Usuarios.setVisible(true);
+    }//GEN-LAST:event_buscar_filtroActionPerformed
+
+    private void cancelar_usuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cancelar_usuarioActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_cancelar_usuarioActionPerformed
 
     /**
      * @param args the command line arguments
@@ -518,15 +676,21 @@ public class BuscarUsuarios extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JDialog Filtrar_Usuarios;
     private javax.swing.JButton Usuario;
+    private javax.swing.JButton buscar_filtro;
     private javax.swing.JButton buscedula;
     private javax.swing.JButton calce;
     private javax.swing.JButton cancelar;
+    private javax.swing.JButton cancelar_usuario;
     private javax.swing.JButton correo;
     private javax.swing.JButton fecha;
     private javax.swing.JPanel filtro;
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton gaurdar_usuario;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel12;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
@@ -536,9 +700,9 @@ public class BuscarUsuarios extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JTabbedPane jTabbedPane1;
-    private javax.swing.JTextField jTextField1;
     private javax.swing.JButton nom;
     private javax.swing.JTable tabla;
     private javax.swing.JButton tel;
@@ -550,6 +714,8 @@ public class BuscarUsuarios extends javax.swing.JFrame {
     private javax.swing.JTextField txtU;
     private javax.swing.JTextField txtUsuario;
     private javax.swing.JTextField txtcedula;
+    private javax.swing.JTextField txtid;
+    private javax.swing.JTextField txtnombre;
     private javax.swing.JButton verdatos;
     // End of variables declaration//GEN-END:variables
 }
