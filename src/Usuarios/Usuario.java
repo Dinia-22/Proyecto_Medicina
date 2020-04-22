65c320ad0bd8ab727d1746b86e131e27c6ff6362
Maria Paula <Maria Paula@Maria>
2020-04-21 21:59:15 -0600

    //programar

--- src/Usuarios/Usuario.java
+++ src/Usuarios/Usuario.java
@@ -9,84 +9,112 @@ import static Conectar.Conectar.conexion;
 import static Conectar.Conectar.datos;
 import static Conectar.Conectar.sentencias;
 import com.toedter.calendar.JDateChooser;
-import java.sql.Connection;
+
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+
 import java.text.SimpleDateFormat;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
-import javax.swing.JTextField;
 
 
 public class Usuario {
 
-    public JTextField ID;
-    public JDateChooser FechaNacimiento;
-    public JTextField txtTel;
-    public JTextField txtCorreo;
-    public JTextField NomUsuario;
-    public JTextField contraseña;
-    public JTextField tipo;
-    public JTextField txtNombre;
+    private int Id;
+    private int FechaNacimiento;
+    private int txtTel;
+    private String txtCorreo;
+    private String NomUsuario;
+    private String contraseña;
+    private String tipo;
+    private String txtNombre;
 
-    public JTextField getID() {
-        return ID;
+    public void setId(int Id) {
+        this.Id = Id;
     }
 
-    public JTextField getNombre() {
-        return txtNombre;
+    public void setFechaNacimiento(int FechaNacimiento) {
+        this.FechaNacimiento = FechaNacimiento;
     }
 
-    public JDateChooser getFechaNacimiento() {
+    public void setTxtTel(int txtTel) {
+        this.txtTel = txtTel;
+    }
+
+    public void setTxtCorreo(String txtCorreo) {
+        this.txtCorreo = txtCorreo;
+    }
+
+    public void setNomUsuario(String NomUsuario) {
+        this.NomUsuario = NomUsuario;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setTxtNombre(String txtNombre) {
+        this.txtNombre = txtNombre;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public int getFechaNacimiento() {
         return FechaNacimiento;
     }
 
-    public JTextField getTel() {
+    public int getTxtTel() {
         return txtTel;
     }
 
-    public JTextField getCorreo() {
+    public String getTxtCorreo() {
         return txtCorreo;
     }
 
-    public JTextField getNomUsuario() {
+    public String getNomUsuario() {
         return NomUsuario;
     }
 
-    public JTextField getContraseña() {
+    public String getContraseña() {
         return contraseña;
     }
 
-    public JTextField getTipo() {
+    public String getTipo() {
         return tipo;
     }
 
+    public String getTxtNombre() {
+        return txtNombre;
+    }
+    
+    
+
     public void create() {
         try {
-            if (this.txtNombre.getText().length() < 25 || this.contraseña.getText().length() < 7 || this.txtCorreo.getText().length() < 30 || this.txtTel.getText().length() < 9 || this.tipo.getText().length() < 20) {
-                PreparedStatement sentencia;
-                sentencia = conexion.prepareStatement("insert usuarios values(null,?,?,?,?,?,?,?)");
-                SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd");
-                sentencia.setString(1, txtNombre.getText());
-                sentencia.setString(2, dt.format(FechaNacimiento.getDate()));
-                sentencia.setString(3, this.txtTel.getText());
-                sentencia.setString(4, this.txtCorreo.getText());
-                sentencia.setString(5, this.NomUsuario.getText());
-                sentencia.setString(6, this.contraseña.getText());
-                sentencia.setString(7, this.tipo.getText());
 
-                sentencia.execute();
+            PreparedStatement sentencia;
+            sentencia = conexion.prepareStatement("insert usuarios values(null,?,?,?,?,?,?,?)");
+            SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd");
+            sentencia.setString(1, txtNombre);
+            sentencia.setInt(2, this.FechaNacimiento);
+            sentencia.setInt(3, this.txtTel);
+            sentencia.setString(4, this.txtCorreo);
+            sentencia.setString(5, this.NomUsuario);
+            sentencia.setString(6, this.contraseña);
+            sentencia.setString(7, this.tipo);
 
-                JOptionPane.showMessageDialog(null, "Se agregaron los datos");
+            sentencia.execute();
 
-            } else {
-                JOptionPane.showMessageDialog(null, "Paso el limite de caracteres");
-            }
+            JOptionPane.showMessageDialog(null, "Se agregaron los datos");
 
         } catch (SQLException ex) {
             Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
@@ -96,14 +124,8 @@ public class Usuario {
     public void update() {
 
         try {
-            if (this.txtNombre.getText().length() < 25 || this.contraseña.getText().length() < 7 || this.txtCorreo.getText().length() < 30 || this.txtTel.getText().length() < 9 || this.tipo.getText().length() < 10) {
-                SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd");
-                sentencias.executeUpdate("update usuarios set NombreCompleto='" + this.txtNombre.getText() + "',FechaNacimiento='" + dt.format(this.FechaNacimiento.getDate()) + "',Telefono='" + this.txtTel.getText() + "',CorreoElectronico='" + this.txtCorreo.getText() + "',NombredeUsuario='" + this.NomUsuario.getText() + "',TipodeUsuario='" + this.tipo.getText() + "' where ID=" + this.ID.getText());
-                JOptionPane.showMessageDialog(null, "Se actualizaron los datos");
-
-            } else {
-                JOptionPane.showMessageDialog(null, "Paso el limite de caracteres");
-            }
+            sentencias.executeUpdate("update usuarios set NombreCompleto='" + this.txtNombre + "',FechaNacimiento='" + this.FechaNacimiento + "',Telefono='" + this.txtTel + "',CorreoElectronico='" + this.txtCorreo + "',NombredeUsuario='" + this.NomUsuario + "',TipodeUsuario='" + this.tipo + "' where ID=" + this.Id);
+            JOptionPane.showMessageDialog(null, "Se actualizaron los datos");
 
         } catch (SQLException ex) {
             Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
@@ -112,13 +134,9 @@ public class Usuario {
 
     public void updateContraseña() {
         try {
-            if (this.contraseña.getText().length() < 15) {
-                sentencias.executeUpdate("update usuarios set contraseña='" + this.contraseña.getText() + "' where id=" + this.ID.getText());
-                JOptionPane.showMessageDialog(null, "Se actualizo la contraseña");
 
-            } else {
-                JOptionPane.showMessageDialog(null, "Paso el limite de caracteres");
-            }
+            sentencias.executeUpdate("update usuarios set contraseña='" + this.contraseña + "' where id=" + this.Id);
+            JOptionPane.showMessageDialog(null, "Se actualizo la contraseña");
 
         } catch (SQLException ex) {
             Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
@@ -128,7 +146,7 @@ public class Usuario {
 
     public void delete() { /// controlar que siempre quede un usuario registrado al momento de eliminar
         try {
-            sentencias.executeUpdate("delete from usuarios where ID=" + this.ID.getText());
+            sentencias.executeUpdate("delete from usuarios where ID=" + this.Id);
 
             JOptionPane.showMessageDialog(null, "dato eliminado");
 
@@ -142,19 +160,14 @@ public class Usuario {
     public void Read_Cedula() {
 
         try {
-            if (this.ID.getText().length() <= 11) {
-                datos = sentencias.executeQuery("select * from usuarios where id='" + this.ID.getText() + "'");
-                if (datos.next()) {
-                    System.out.println(datos.getInt(1));
-                    JOptionPane.showMessageDialog(null, "Dato Encontrado " + datos.getString(1));
-                    System.out.println(datos.getString(3));
-                } else {
-                    JOptionPane.showMessageDialog(null, "No hay mas registros");
-                }
 
+            datos = sentencias.executeQuery("select * from usuarios where id='" + this.Id + "'");
+            if (datos.next()) {
+                System.out.println(datos.getInt(1));
+                JOptionPane.showMessageDialog(null, "Dato Encontrado " + datos.getString(1));
+                System.out.println(datos.getString(3));
             } else {
-                JOptionPane.showMessageDialog(null, "Se paso del limite de caracteres");
-
+                JOptionPane.showMessageDialog(null, "No hay mas registros");
             }
 
         } catch (SQLException ex) {
@@ -164,18 +177,14 @@ public class Usuario {
 
     public void readNombre() {
         try {
-            if (this.txtNombre.getText().length() <= 25) {
-                datos = sentencias.executeQuery("select * from usuarios where NombreCompleto='" + this.txtNombre.getText() + "'");
-                if (datos.next()) {
-                    System.out.println(datos.getInt(1));
-                    JOptionPane.showMessageDialog(null, "Dato Encontrado " + datos.getString(2));
-                    System.out.println(datos.getString(3));
-                } else {
-                    JOptionPane.showMessageDialog(null, "No hay mas registros");
-                }
-            } else {
-                JOptionPane.showMessageDialog(null, "Paso el limite de caracteres");
 
+            datos = sentencias.executeQuery("select * from usuarios where NombreCompleto='" + this.txtNombre + "'");
+            if (datos.next()) {
+                System.out.println(datos.getInt(1));
+                JOptionPane.showMessageDialog(null, "Dato Encontrado " + datos.getString(2));
+                System.out.println(datos.getString(3));
+            } else {
+                JOptionPane.showMessageDialog(null, "No hay mas registros");
             }
 
         } catch (SQLException ex) {
@@ -186,9 +195,8 @@ public class Usuario {
 
     public void readFecha() {
         try {
-            SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd");
 
-            datos = sentencias.executeQuery("select * from usuarios where FechaNacimiento='" + dt.format(this.FechaNacimiento.getDate()) + "'");
+            datos = sentencias.executeQuery("select * from usuarios where FechaNacimiento='" + this.FechaNacimiento + "'");
             if (datos.next()) {
 
                 System.out.println(datos.getInt(1));
@@ -207,19 +215,14 @@ public class Usuario {
 
     public void readTel() {
         try {
-            if (this.txtTel.getText().length() <= 9) {
-                datos = sentencias.executeQuery("select * from usuarios where Telefono='" + this.txtTel.getText() + "'");
-                if (datos.next()) {
-                    System.out.println(datos.getInt(1));
-                    JOptionPane.showMessageDialog(null, "Dato Encontrado " + datos.getString(4));
-                    System.out.println(datos.getString(3));
-                } else {
-                    JOptionPane.showMessageDialog(null, "No hay mas registros");
-                }
 
+            datos = sentencias.executeQuery("select * from usuarios where Telefono='" + this.txtTel + "'");
+            if (datos.next()) {
+                System.out.println(datos.getInt(1));
+                JOptionPane.showMessageDialog(null, "Dato Encontrado " + datos.getString(4));
+                System.out.println(datos.getString(3));
             } else {
-                JOptionPane.showMessageDialog(null, "Paso el limite de caracteres");
-
+                JOptionPane.showMessageDialog(null, "No hay mas registros");
             }
 
         } catch (SQLException ex) {
@@ -229,18 +232,14 @@ public class Usuario {
 
     public void readCorreo() {
         try {
-            if (this.txtCorreo.getText().length() <= 30) {
-                datos = sentencias.executeQuery("select * from usuarios where CorreoElectronico='" + this.txtCorreo.getText() + "'");
-                if (datos.next()) {
-                    System.out.println(datos.getInt(1));
-                    JOptionPane.showMessageDialog(null, "Dato Encontrado " + datos.getString(5));
-                    System.out.println(datos.getString(3));
-                } else {
-                    JOptionPane.showMessageDialog(null, "No hay mas registros");
-                }
 
+            datos = sentencias.executeQuery("select * from usuarios where CorreoElectronico='" + this.txtCorreo + "'");
+            if (datos.next()) {
+                System.out.println(datos.getInt(1));
+                JOptionPane.showMessageDialog(null, "Dato Encontrado " + datos.getString(5));
+                System.out.println(datos.getString(3));
             } else {
-                JOptionPane.showMessageDialog(null, "Paso el limite de caracteres");
+                JOptionPane.showMessageDialog(null, "No hay mas registros");
             }
 
         } catch (SQLException ex) {
@@ -251,18 +250,14 @@ public class Usuario {
 
     public void readUsuario() {
         try {
-            if (this.NomUsuario.getText().length() <= 15) {
-                datos = sentencias.executeQuery("select * from usuarios where NombredeUsuario='" + this.NomUsuario.getText() + "'");
-                if (datos.next()) {
-                    System.out.println(datos.getInt(1));
-                    JOptionPane.showMessageDialog(null, "Dato Encontrado " + datos.getString(6));
-                    System.out.println(datos.getString(3));
-                } else {
-                    JOptionPane.showMessageDialog(null, "No hay mas registros");
-                }
-            } else {
-                JOptionPane.showMessageDialog(null, "Paso el limite de caracteres");
 
+            datos = sentencias.executeQuery("select * from usuarios where NombredeUsuario='" + this.NomUsuario + "'");
+            if (datos.next()) {
+                System.out.println(datos.getInt(1));
+                JOptionPane.showMessageDialog(null, "Dato Encontrado " + datos.getString(6));
+                System.out.println(datos.getString(3));
+            } else {
+                JOptionPane.showMessageDialog(null, "No hay mas registros");
             }
 
         } catch (SQLException ex) {
@@ -273,18 +268,14 @@ public class Usuario {
 
     public void readTipo() {
         try {
-            if (this.tipo.getText().length() <= 10) {
-                datos = sentencias.executeQuery("select * from usuarios where TipodeUsuario='" + this.tipo.getText() + "'");
-                if (datos.next()) {
-                    System.out.println(datos.getInt(1));
-                    JOptionPane.showMessageDialog(null, "Dato Encontrado " + datos.getString(8));
-                    System.out.println(datos.getString(3));
-                } else {
-                    JOptionPane.showMessageDialog(null, "No hay mas registros");
-                }
 
+            datos = sentencias.executeQuery("select * from usuarios where TipodeUsuario='" + this.tipo + "'");
+            if (datos.next()) {
+                System.out.println(datos.getInt(1));
+                JOptionPane.showMessageDialog(null, "Dato Encontrado " + datos.getString(8));
+                System.out.println(datos.getString(3));
             } else {
-                JOptionPane.showMessageDialog(null, "Paso el limite de caracteres");
+                JOptionPane.showMessageDialog(null, "No hay mas registros");
             }
 
         } catch (SQLException ex) {
@@ -293,8 +284,5 @@ public class Usuario {
 
     }
 
-//    public static void main(String[] args) {
-//        Conectar p = new Conectar();
-//        p.conectar();
-//    }
+
 }
