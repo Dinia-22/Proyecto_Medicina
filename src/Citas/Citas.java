6d827c9d8c14c8614c3e3505deed6293e3cc201d
Maria Paula <Maria Paula@Maria>
2020-04-26 09:53:54 -0600

    //programar

--- src/Citas/Citas.java
+++ src/Citas/Citas.java
@@ -90,7 +90,7 @@ public class Citas extends Paciente {
 
         } catch (SQLException e) {
             if (e.getErrorCode() == 1062) {
-                JOptionPane.showMessageDialog(null, "La hora ya existe ", "La cedula ya existe", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(null, "La hora ya existe ", "No puede introducir la misma fecha", JOptionPane.ERROR_MESSAGE);
                 System.out.println("");
 
             }
