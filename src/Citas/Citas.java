f69b76d72fd0a0600a9c89f8c7cb1b91ee72e3a2
Maria Paula <Maria Paula@Maria>
2020-04-27 14:49:58 -0600

    //programar

--- src/Citas/Citas.java
+++ src/Citas/Citas.java
@@ -90,7 +90,8 @@ public class Citas extends Paciente {
 
         } catch (SQLException e) {
             if (e.getErrorCode() == 1062) {
-                JOptionPane.showMessageDialog(null, "La hora ya existe y fecha existen ", null, JOptionPane.ERROR_MESSAGE);
+                
+                JOptionPane.showMessageDialog(null, "La hora,fecha,cedula ya existen ", null, JOptionPane.ERROR_MESSAGE);
                 
 
             }
